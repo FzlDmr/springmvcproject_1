12a7ad3708d78cb048e8247ecaa05e452cc4b568
Fazıl Demir <fazildemir20@gmail.com>
2023-06-12 14:19:14 +0300

    search feature added

--- src/main/java/com/dev3/service/CustomerServiceImpl.java
+++ src/main/java/com/dev3/service/CustomerServiceImpl.java
@@ -42,4 +42,9 @@ public class CustomerServiceImpl implements CustomerService{
     public void deleteCustomer(int theId) {
         customerDao.deleteCustomer(theId);
     }
+
+    @Override
+    public List<Customer> searchCustomers(String theSearchName) {
+        return customerDao.searchCustomers(theSearchName);
+    }
 }
